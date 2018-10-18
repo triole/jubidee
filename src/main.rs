ba73de88d82bb463c13f25837ad7186dd69bb7a9
Olaf Michaelis <omichaelis@aip.de>
2018-10-18 12:54:16 +0200

    Make code more rusty

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,11 @@ extern crate chrono;
 mod people;
 
 fn main() {
-    let data = people::data();
+    let mut data = people::Data::init();
+    data.add("Matea", "2017-12-30 17:40:00");
+    data.add("Flora", "2016-02-20 20:42:00");
+    data.add("Bär", "1981-11-22 12:00:00");
+    data.add("Er", "1981-08-07 11:55:00");
+
     println!("{:#?}", data);
 }
--- src/people.rs
+++ src/people.rs
@@ -1,38 +1,66 @@
 use chrono;
-use chrono::prelude::*;
 use chrono::offset::{Local, TimeZone};
+use chrono::prelude::*;
 
 use std::cmp::Ordering;
 
-pub fn data() -> Vec<Person>{
-    let mut vec = Vec::new();
-    vec.push(Person::new("Matea", "2017-12-30 17:40:00"));
-    vec.push(Person::new("Flora", "2016-02-20 20:42:00"));
-    vec.push(Person::new("Bär", "1981-11-22 12:00:00"));
-    vec.push(Person::new("Er", "1981-08-07 11:55:00"));
-    vec.sort();
-    return vec;
+#[derive(Debug)]
+pub struct Data {
+    now: chrono::DateTime<chrono::Local>,
+    people: Vec<Person>,
 }
 
-#[derive(Debug)]
-#[derive(Eq)]
+impl Data {
+    pub fn init() -> Self {
+        let d = Data {
+            now: Local::now(),
+            people: Vec::new(),
+        };
+        return d;
+    }
+
+    pub fn add(&mut self, name: &str, dob: &str) {
+        let dob_date = self.parse_date_string(dob);
+        let p = Person {
+            name: name.to_string(),
+            dob: dob_date,
+            age: self.date_delta(&self.now, &dob_date),
+            next_birthday: self.next_birthday(&dob_date),
+        };
+        self.people.push(p);
+    }
+
+    fn parse_date_string(&self, s: &str) -> chrono::DateTime<chrono::Local> {
+        return Local.datetime_from_str(&s, "%Y-%m-%d %H:%M:%S").unwrap();
+    }
+
+    fn date_delta(
+        &self,
+        d1: &chrono::DateTime<chrono::Local>,
+        d2: &chrono::DateTime<chrono::Local>,
+    ) -> chrono::Duration {
+        return d2.signed_duration_since(d1.to_owned());
+    }
+
+    fn next_birthday(
+        &self,
+        dob: &chrono::DateTime<chrono::Local>,
+    ) -> chrono::DateTime<chrono::Local> {
+        let current_year = self.now.year();
+        let mut next_birthday = dob.with_year(current_year).unwrap();
+        if next_birthday < self.now {
+            next_birthday = dob.with_year(current_year + 1).unwrap();
+        }
+        return next_birthday;
+    }
+}
+
+#[derive(Debug, Eq)]
 pub struct Person {
     name: String,
     dob: chrono::DateTime<chrono::Local>,
     age: chrono::Duration,
     next_birthday: chrono::DateTime<chrono::Local>,
-}
-
-impl Person {
-    fn new(name: &str, dob: &str) -> Person {
-        let dob_date = parse_date_string(dob);
-        Person {
-            name: name.to_string(),
-            dob: dob_date,
-            age: date_delta(&Local::now(), &dob_date),
-            next_birthday: next_birthday(&dob_date)
-        }
-    }
 }
 
 // --- make the struct sortable by next birthday
@@ -52,22 +80,4 @@ impl PartialEq for Person {
     fn eq(&self, other: &Person) -> bool {
         self.next_birthday == other.next_birthday
     }
-}
-// --- end sortable
-
-fn parse_date_string(s: &str)-> chrono::DateTime<chrono::Local>{
-    return Local.datetime_from_str(&s, "%Y-%m-%d %H:%M:%S").unwrap();
-}
-
-fn date_delta(d1: &chrono::DateTime<chrono::Local>, d2: &chrono::DateTime<chrono::Local>) -> chrono::Duration{
-    return d2.signed_duration_since(d1.to_owned());
-}
-
-fn next_birthday(dob: &chrono::DateTime<chrono::Local>) -> chrono::DateTime<chrono::Local>{
-    let current_year = Local::now().year();
-    let mut next_birthday = dob.with_year(current_year).unwrap();
-    if next_birthday < Local::now() {
-        next_birthday = dob.with_year(current_year+1).unwrap();
-    }
-    return next_birthday
 }
